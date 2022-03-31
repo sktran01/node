d119c3f5fa5f2e351676497e6a00f5bc56d32f6b
sktran01 <sktrn88@yahoo.com>
2022-03-31 15:31:23 -0500

    more prompt

--- app.js
+++ app.js
@@ -20,17 +20,49 @@ return inquirer
         {
             type:'input',
             name:'name',
-            message:'What is your name?'
+            message:'What is your name? (Required)',
+            validate: nameInput=>{
+                if (nameInput){
+                    return true;
+                }else{
+                    console.log('please enter your name!');
+                    return false;
+                }
+            }
+
+
         },
         {
             type:'input',
             name:'github',
-            message:'Enter your GitHub Username'
+            message:'Enter your GitHub Username (required)',
+            validate: nameInput=>{
+                if (nameInput){
+                    return true;
+                }else{
+                    console.log('please enter GitHub Username!');
+                    return false;
+                }
+            }    
         },
+        {
+            type:'confirm',
+            name: 'confirmAbout',
+            message:'Would you like to enter some information about yourself for an "about" section?',
+            default:true
+        },
+
         {
             tyoe:'input',
             name:'about',
-            message:'Provide some information about yourself:'
+            message:'Provide some information about yourself:',
+            when:({confirmAbout})=>{
+                if (confirmAbout){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
         }
 
     ]);
@@ -38,12 +70,16 @@ return inquirer
 };
 
 
-const promptProject = () => {
+const promptProject = portfolioData=> {
     console.log(`
   =================
   Add a New Project
   =================
   `);
+// If there's no 'projects' array property, create one
+if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
     return inquirer.prompt([
       {
         type: 'input',
@@ -78,9 +114,22 @@ const promptProject = () => {
         message: 'Would you like to enter another project?',
         default: false
       }
-    ]);
-  };
-  promptUser()
-  .then(answers => console.log(answers))
+      
+    ])
+    .then(projectData =>{
+        portfolioData.projects.push(projectData)
+        if(projectData.confirmAddProject){
+            return promptProject(portfolioData);
+        }else {
+            return portfolioData;
+        }
+    })
+    
+};
+
+    
+promptUser()
   .then(promptProject)
-  .then(projectAnswers => console.log(projectAnswers));
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
